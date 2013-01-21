ea9950418434e948d3cd8942e0abcf0def7b4f9f
David McClure <david.mcclure@virginia.edu>
2013-01-21 12:28:02 -0500

    Blocks in basic structure.

--- svg-to-wkt.js
+++ svg-to-wkt.js
@@ -11,21 +11,45 @@
   var SVGtoWKT = {};
   var root = this;
 
-  // TODO
+  // Alias the original jQuery variable.
+  var $ = root.$;
 
-  // AMD / RequireJS
+  // If we're in Node.js, import jQuery.
+  if (typeof $ == 'undefined') $ = require('jquery');
+
+  // Expose AMD / RequireJS module.
   if (typeof define !== 'undefined' && define.amd) {
-    define('async', [], function() {
+    define('SVGtoWKT', ['jquery'], function() {
       return SVGtoWKT;
     });
   }
 
-  // Node.js
+  // Export Node.js module.
   else if (typeof module !== 'undefined' && module.exports) {
     module.exports = SVGtoWKT;
   }
 
-  // Browser
+  // If we're in the browser, add `SVGtoWKT` as a global.
   else root.SVGtoWKT = SVGtoWKT;
+
+
+  SVGtoWKT.convert = function(svg) {
+
+    var wkt = 'GEOMETRYCOLLECTION(';
+    var els = [];
+
+    // Parse the raw XML.
+    var xml = $($.parseXML(svg));
+
+    // POLYGON
+    xml.find('polygon').each(function(p) {
+      // TODO|dev
+      console.log(p);
+    });
+
+    return wkt + ')';
+
+  };
+
 
 })();

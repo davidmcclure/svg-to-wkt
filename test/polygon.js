366bfffa6016fc9c744543e244a48f31046cab88
David McClure <davidwilliammcclure@gmail.com>
2014-06-01 14:31:23 -0400

    Adds Mocha testing rig.

---
+++ test/polygon.js
@@ -0,0 +1,15 @@
+
+/* vim: set expandtab tabstop=2 shiftwidth=2 softtabstop=2 cc=80: */
+
+
+var SVGtoWKT = require('../svg-to-wkt');
+var assert = require('chai').assert;
+
+
+describe('SVGtoWKT.polygon()', function() {
+
+  it('test', function() {
+    assert.isTrue(true);
+  });
+
+});
